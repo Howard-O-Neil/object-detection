1c35e8e246cb704bd0648ebeaebe8c453ffe551b
mingkhoi <mingkhoitran1234@gmail.com>
2022-01-31 18:56:42 +0700

    preprocess imgs for prediction

--- test_imagenet.py
+++ test_imagenet.py
@@ -10,45 +10,42 @@ import model_cifa10.file_utils as file_utils
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
-MODEL_ID = 3
+MODEL_ID = 1
+
+LABEL_PATH = "data/imagenet/LOC_synset_mapping.txt"
+labels = numpy.array([["", ""]])
+
+f = os.open(LABEL_PATH, os.O_RDONLY)
+res = os.read(f, os.path.getsize(LABEL_PATH)).decode("utf-8")
+
+for label_str in res.split("\n")[0:-1]:
+    arr = numpy.expand_dims(numpy.array(label_str.split(" ", 1)), 0)
+    labels = np.concatenate((labels, arr), axis=0)
+
+labels = labels[1:, :]
+
 
 # t_train_size = 100
 # t_test_size = 32
 
-names, data, labels = file_utils.read_data(\
+_, data, _ = file_utils.read_data(\
     "data/cifa10", is_train=True)
 
 # data = data[5000:5000+t_train_size]
 # labels = labels[5000:5000+t_train_size]
 
-t_names, t_data, t_labels = file_utils.read_data(\
+_, t_data, _ = file_utils.read_data(\
     "data/cifa10", is_train=False)
 
-# t_data = t_data[1000:1000+t_test_size]
-# t_labels = t_labels[1000:1000+t_test_size]
+# not required training
+# required download weights from google apis
+from model_imagenet.model import predict_model
+import model_imagenet.tf_preprocess_img as tf_preproc
 
-evaluation_size = 1000
-evaluation_pos = 2000
-t_data_eva = t_data[evaluation_pos:evaluation_pos+evaluation_size]
-t_labels_eva = t_labels[evaluation_pos:evaluation_pos+evaluation_size]
+predict_res = numpy.argmax(
+    predict_model.predict_on_batch(tf_preproc.tf_preprocess_images(data[5:5+16], True)),
+    1
+)
 
-y_train_one_hot = file_utils.convert_to_one_hot(labels, len(names))
-y_test_one_hot = file_utils.convert_to_one_hot(t_labels, len(t_names))
-y_test_one_hot_eva = file_utils.convert_to_one_hot(t_labels_eva, len(t_names))
+print(labels[predict_res, 1:])
 
-
-from model_cifa10.model import Model
-
-ml_model = Model(MODEL_ID, "meta")
-ml_model.init_session()
-# ml_model.restore_session("")
-
-try:
-    ml_model.training_loop(data, labels, y_train_one_hot, t_data_eva, t_labels_eva, y_test_one_hot_eva)
-except KeyboardInterrupt:
-    ml_model.save_model()
-    ml_model.save_metrics()
-
-    exit(1)
-else:
-    print("UNKNOWN ERROR!!!")
