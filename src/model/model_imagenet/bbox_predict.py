ef40fc1b7269a1adf9657a523b59edd2817bc890
mingkhoi <mingkhoitran1234@gmail.com>
2022-02-13 09:34:48 +0700

    tune loss function; remove tanh activation; switch to old approach

--- src/model/model_imagenet/bbox_predict.py
+++ src/model/model_imagenet/bbox_predict.py
@@ -39,11 +39,11 @@ class Kaming_he_dense(keras.layers.Layer):
         if self.activation:
             if training:
                 return tf.nn.dropout(
-                    tf.nn.tanh(tf.nn.bias_add(tf.matmul(inputs, self.w), self.b)),
+                    tf.nn.bias_add(tf.matmul(inputs, self.w), self.b),
                     rate=self.dropout_rate,
                 )
             else:
-                return tf.nn.tanh(tf.nn.bias_add(tf.matmul(inputs, self.w), self.b))
+                return tf.nn.bias_add(tf.matmul(inputs, self.w), self.b)
         else:
             return tf.nn.bias_add(tf.matmul(inputs, self.w), self.b)
 
@@ -57,7 +57,7 @@ class Bbox_predict:
 
     img_batch_size = 1
     train_batch_size = 16
-    _lambda = 25.
+    _lambda = 2.5
     epochs = 50
 
     def __init__(self):
@@ -82,7 +82,7 @@ class Bbox_predict:
         )
         self.model.build((None, 25088))
 
-        self.optimizer = keras.optimizers.SGD(learning_rate=0.000001, momentum=0.9)
+        self.optimizer = keras.optimizers.SGD(learning_rate=0.00000001, momentum=0.9)
         self.model_path = f"""{os.getenv("model_path")}/checkpoint"""
 
         self.logger = logging.getLogger("r-cnn logger")
@@ -136,13 +136,13 @@ class Bbox_predict:
         tw = tf.math.log(tf.math.divide(gt_w, propose_w))
         th = tf.math.log(tf.math.divide(gt_h, propose_h))
 
-        lx = tf.math.subtract(tx, predictions[:, 0])
-        ly = tf.math.subtract(ty, predictions[:, 1])
-        lw = tf.math.subtract(tw, predictions[:, 2])
-        lh = tf.math.subtract(th, predictions[:, 3])
+        lx = tf.reduce_sum(tf.math.square(tf.math.subtract(tx, predictions[:, 0])))
+        ly = tf.reduce_sum(tf.math.square(tf.math.subtract(ty, predictions[:, 1])))
+        lw = tf.reduce_sum(tf.math.square(tf.math.subtract(tw, predictions[:, 2])))
+        lh = tf.reduce_sum(tf.math.square(tf.math.subtract(th, predictions[:, 3])))
 
-        loss = tf.math.square(tf.stack([lx, ly, lw, lh], axis=1))
-        loss = tf.reduce_mean(tf.reduce_min(loss, axis=1))
+        loss = tf.reduce_mean(tf.constant([lx, ly, lw, lh]))
+
         return loss
 
     # loss function base on 2nd approach
@@ -154,13 +154,13 @@ class Bbox_predict:
 
     @tf.function(experimental_relax_shapes=True)
     # pure Tensor operations, no numpy
-    def train_step(self, train_batch_y, train_batch_img):
+    def train_step(self, train_batch_x, train_batch_y, train_batch_img):
         with tf.GradientTape() as tape:
             feature_maps = self.img_cnn_model(train_batch_img)
             bbox_predicts = self.model(feature_maps, training=True)
 
-            # loss = self.loss_fn_aprch1(train_batch_x, train_batch_y, bbox_predicts)
-            loss = self.loss_fn_aprch2(train_batch_y, bbox_predicts)
+            loss = self.loss_fn_aprch1(train_batch_x, train_batch_y, bbox_predicts)
+            # loss = self.loss_fn_aprch1(train_batch_y, bbox_predicts)
 
             total_regularized_loss = tf.constant(0.0)
             for reg_loss in self.model.losses:
@@ -175,19 +175,19 @@ class Bbox_predict:
 
     @tf.function(experimental_relax_shapes=True)
     # pure Tensor operations, no numpy
-    def validate_step(self, train_batch_y, train_batch_img):
+    def validate_step(self, train_batch_x, train_batch_y, train_batch_img):
         feature_maps = self.img_cnn_model(train_batch_img)
         bbox_predicts = self.model(feature_maps, training=False)
 
-        # loss = self.loss_fn_aprch1(train_batch_x, train_batch_y, bbox_predicts)
-        loss = self.loss_fn_aprch2(train_batch_y, bbox_predicts)
+        loss = self.loss_fn_aprch1(train_batch_x, train_batch_y, bbox_predicts)
+        # loss = self.loss_fn_aprch1(train_batch_y, bbox_predicts)
 
         return loss
 
     def enumerate_batch(
         self, prefix_str, epoch_id, batch_train, train_batch_size, callback, is_log = True
     ):
-        # batch_x = np.array([])
+        batch_x = np.array([])
         batch_y = np.array([])
         batch_img = np.array([])
 
@@ -202,30 +202,30 @@ class Bbox_predict:
             gt = self.ground_truth_bbbox[filter_ids]
             img_bboxs = imu.extract_bboxs(imgs[0], propose)
 
-            if batch_y.shape[0] <= 0:
-                # batch_x = propose
+            if batch_x.shape[0] <= 0:
+                batch_x = propose
                 batch_y = gt
                 batch_img = img_bboxs
             else:
-                # batch_x = np.concatenate((batch_x, propose), axis=0)
+                batch_x = np.concatenate((batch_x, propose), axis=0)
                 batch_y = np.concatenate((batch_y, gt), axis=0)
                 batch_img = np.concatenate((batch_img, img_bboxs), axis=0)
 
-        total_train_batch = batch_y.shape[0] // train_batch_size + 1
+        total_train_batch = batch_x.shape[0] // train_batch_size + 1
 
-        if batch_y.shape[0] % train_batch_size == 0:
+        if batch_x.shape[0] % train_batch_size == 0:
             total_train_batch -= 1
 
         mean_loss = 0.
         for k in range(total_train_batch):
-            offset = (k * train_batch_size) % batch_y.shape[0]
+            offset = (k * train_batch_size) % batch_x.shape[0]
 
-            # train_batch_x = batch_x[offset : offset + train_batch_size]
+            train_batch_x = batch_x[offset : offset + train_batch_size]
             train_batch_y = batch_y[offset : offset + train_batch_size]
             train_batch_img = batch_img[offset : offset + train_batch_size]
 
-            # loss = callback(train_batch_x, train_batch_y, train_batch_img).numpy()
-            loss = callback(train_batch_y, train_batch_img).numpy()
+            loss = callback(train_batch_x, train_batch_y, train_batch_img).numpy()
+            # loss = callback(train_batch_y, train_batch_img).numpy()
             mean_loss += loss
 
             if is_log:
@@ -257,10 +257,15 @@ class Bbox_predict:
             img_bboxs = imu.extract_bboxs(scale_img, sub_bboxs)
 
             compute_bboxs = self.model(self.img_cnn_model(img_bboxs), training=False).numpy()
+            generate_bboxs = np.copy(sub_bboxs)
+            generate_bboxs[:, 0] = np.add(np.multiply(sub_bboxs[:, 2], compute_bboxs[:, 0]), sub_bboxs[:, 0])
+            generate_bboxs[:, 1] = np.add(np.multiply(sub_bboxs[:, 3], compute_bboxs[:, 1]), sub_bboxs[:, 1])
+            generate_bboxs[:, 2] = np.multiply(sub_bboxs[:, 2], np.exp(compute_bboxs[:, 2]))
+            generate_bboxs[:, 3] = np.multiply(sub_bboxs[:, 3], np.exp(compute_bboxs[:, 3]))
             
             if len(new_bboxs.shape) <= 1:
-                new_bboxs = compute_bboxs
-            else: new_bboxs = np.concatenate((new_bboxs, compute_bboxs), axis=0)
+                new_bboxs = generate_bboxs
+            else: new_bboxs = np.concatenate((new_bboxs, generate_bboxs), axis=0)
 
         return new_bboxs
 
--- src/train.py
+++ src/train.py
@@ -43,3 +43,14 @@ bbox_model.assign_img_list_validation(val_list)
 bbox_model.assign_bbox_dataset(x_DF, y_DF, imgs_DF)
 
 bbox_model.train_loop(transfer=False)
+# try:
+#     bbox_model.train_loop(transfer=False)
+
+# # Google Colab support throw KeyboardInterrupt error when interupt kernel
+# except KeyboardInterrupt:
+#     bbox_model.save_model()
+
+#     print("===== SAVE MODEL SUCCESSFULLY =====")
+#     exit(0)
+# else:
+#     print("UNKNOWN ERROR!!!")
